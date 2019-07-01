4e1688f1505df0ceed78e068596037ec2cde7a4e
Peter Nguyen <nguyenvandung02051999@gmail.com>
2019-07-01 06:24:42 -0700

    First problem, two sum

---
+++ TwoSum/src/TwoSum.cpp
@@ -0,0 +1,19 @@
+/*
+ * TwoSum.cpp
+ *
+ *  Created on: Jun 30, 2019
+ *      Author: dungnguyen
+ */
+
+#include <iostream>
+
+void quicksort(int array[]);
+
+int main()	{
+	std::cout << "Hello world\n";
+	return 0;
+}
+
+void quicksort(int array[])	{
+
+}
