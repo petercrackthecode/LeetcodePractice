98c5d6e575c654deae064f0b72e14b7e29f051da
petercrackthecode <nguyenvandung02051999@gmail.com>
2020-01-06 06:41:38 -0800

    Added the ZigZagConversion problem

---
+++ ZigZagConversion/ZigZagConversion.cpp
@@ -0,0 +1,15 @@
+// Problem link: https://leetcode.com/problems/zigzag-conversion/
+string convert(string s, int numRows) {
+
+}
+
+int main() {
+	string s;
+	int numRows;
+	std::cout << "Enter a string: ";
+	std::cin >> s;
+	std::cout << "Enter the number of rows: "
+	std::cin >> numRows;
+	std::cout << "Newly converted string is " << convert(s, numRows) << "\n";
+	return 0;
+}
