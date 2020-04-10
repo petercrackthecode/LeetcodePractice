94bd905ce4c24a4585086e9cc8c111a037b208f6
petercrackthecode <nguyenvandung02051999@gmail.com>
2020-04-10 01:11:31 -0700

    solved this problem by clues. This is an interesting one.

--- maxProfit.cpp
+++ maxProfit.cpp
@@ -1,5 +1,10 @@
 // link: https://leetcode.com/explore/featured/card/30-day-leetcoding-challenge/528/week-1/3287/
 
+// Solution 1: Brute Force
+// Traverse through the array, calculate all the possibilities, and use recursion to find the highest price.
+// Time Complexity: O(n^n). Recursive function is called n^n times
+// Space complexity: O(n). Depth of recursion is n.
+
 int maxProfit(std::vector<int> &prices, int startPos)
 {
     int highestProfit{0}, buyPos = startPos;
@@ -33,3 +38,41 @@ int maxProfit(std::vector<int> &prices)
 {
     return maxProfit(prices, 0);
 }
+
+// Solution 2: Peak Valley Approach
+// Time complexity: O(n). Single pass
+// Space complexity: O(1). Constant space required.
+int maxProfit(std::vector<int> prices) {
+    int i{0},
+        valley= prices[0],
+        peak= prices[0],
+        maxProfit= 0;
+
+    while (i < prices.size() - 1) {
+        while (i < prices.size() - 1 && prices[i] >= prices[i + 1])
+            ++i;
+        valley= prices[i];
+
+        while (i < prices.size() - 1 && prices[i] <= prices[i + 1])
+            ++i;
+        peak= prices[i];
+
+        maxProfit+= peak - valley;
+    }
+
+    return maxProfit;
+}
+
+// Solution 3: Simple One Pass
+int maxProfit(std::vector<int> prices) {
+    int highestProfit{0};
+
+    for (int i= 0; i < prices.size(); ++i) {
+        if (prices[i] > prices[i - 1])
+            highestProfit+= prices[i] - prices[i - 1];
+    }
+
+    return highestProfit;
+}
+// Time complexity: O(n), single pass.
+// Space complexity: O(1). Constant space needed.
