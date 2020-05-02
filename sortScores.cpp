d0e83ddba16898f6a5b5536b2f0a81e7a3a2babc
petercrackthecode <nguyenvandung02051999@gmail.com>
2020-05-02 00:03:30 -0700

    Working on my own solution

---
+++ sortScores.cpp
@@ -0,0 +1,8 @@
+// link: https://www.interviewcake.com/question/cpp/top-scores?course=fc1&section=hashing-and-hash-tables
+std::vector<int> sortScores(const std::vector<int>& unorderedScores, int highestPossibleScore)
+{
+    // sort the scores in O(n) time
+    
+
+    return std::vector<int>();
+}
