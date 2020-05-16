6a645d27401ac0127027ec115accb151bef74579
petercrackthecode <nguyenvandung02051999@gmail.com>
2020-05-16 00:40:50 -0700

    added isCousins.cpp

---
+++ isCousins.cpp
@@ -0,0 +1,16 @@
+// link: https://leetcode.com/problems/cousins-in-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+bool isCousins(TreeNode* root, int x, int y) {
+        
+}
