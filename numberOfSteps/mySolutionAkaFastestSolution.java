407243c5896b7c6e452eadd065ed8305bdb118ff
petercrackthecode <nguyenvandung02051999@gmail.com>
2021-02-12 10:57:26 -0800

    One easy problem solved in Java

---
+++ numberOfSteps/mySolutionAkaFastestSolution.java
@@ -0,0 +1,13 @@
+// link: https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+class Solution {
+    public int numberOfSteps (int num) {
+        Integer steps = 0;
+        
+        while (num > 0) {
+            num = (num % 2 == 0) ? (num / 2) : (num - 1);
+            ++steps;
+        }
+        
+        return steps;
+    }
+}
