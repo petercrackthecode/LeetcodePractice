a4df0d4c79b1c66034a14065dc8ad9467b5842c9
petercrackthecode <nguyenvandung02051999@gmail.com>
2021-03-20 00:58:27 -0700

    Update the fastest solution to be even faster

--- canVisitAllRooms/fastestSolution.java
+++ canVisitAllRooms/fastestSolution.java
@@ -1,28 +1,25 @@
 // link: https://leetcode.com/problems/keys-and-rooms/
 class Solution {
+    private boolean[] visitedRooms = new boolean[rooms.size()];
+
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        boolean[] visitedRooms = new boolean[rooms.size()];
-        moveBetweenRooms(rooms, 0, visitedRooms);
-        
-        for(boolean isRoomVisited: visitedRooms) {
-            if(!isRoomVisited) {
+        moveBetweenRooms(rooms, 0);
+
+        for (boolean isRoomVisited : visitedRooms) {
+            if (!isRoomVisited)
                 return false;
-            }
         }
-        
+
         return true;
     }
-    
-    private void moveBetweenRooms(List<List<Integer>> rooms, int curRoom, boolean[] visitedRooms) {
-        
-        if(visitedRooms[curRoom]) {
+
+    private void moveBetweenRooms(List<List<Integer>> rooms, int currRoom) {
+        if (visitedRooms[currRoom])
             return;
-        }
-        visitedRooms[curRoom] = true;
-        List<Integer> accessibleRooms = rooms.get(curRoom);
-        for(int room: accessibleRooms) {
-            moveBetweenRooms(rooms, room, visitedRooms);
-        }
+        visitedRooms[currRoom] = true;
+        List<Integer> accessibleRooms = rooms.get(currRoom);
+        for (int room : accessibleRooms)
+            moveBetweenRooms(rooms, room);
     }
 }
 
