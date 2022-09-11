c59d5dcf1cd4b17ceeb3adc42fb7c79f2f4313c8
Peter Nguyen <peternguyenforwork@gmail.com>
2022-09-11 14:57:16 -0700

    completed 01_matrix with the most space efficient solution. Quite an interesting problem.

--- 01_matrix/my_solution.py
+++ 01_matrix/my_solution.py
@@ -1,54 +1,54 @@
-# https://leetcode.com/problems/01-matrix/
+from collections import deque
 from typing import List
 
 
+def calculate_nearest_zero_distance(zero_distance_mat: List[List[int]], r: int, c: int, nearest_distance, assigned_value_cells: deque) -> None:
+    zero_distance_mat[r][c] = nearest_distance + 1
+    assigned_value_cells.append((r, c))
+
+
 class Solution:
-    def __init__(self):
-        self.distance_matrix = list()
-        # is_visited: check if the element is already visited, so not to recursively call it again.
-        # key: a pair of tuple
-        # value: bool
-        self.is_visited = dict()
-
-    def getCellDistanceToNearestZero(self, mat: List[List[int]], r: int, c: int) -> int:
-        # Only update the self.distance_matrix at the [r][c] position if it hasn't been updated yet.
-        print('row = ', r, ', column = ', c)
-        print("mat = ", mat)
-        if self.distance_matrix[r][c] == -1:
-            columns, rows = len(mat[0]), len(mat)
-            LARGEST_POSSIBLE_DISTANCE = rows * columns - 1
-            if mat[r][c] == 0:
-                self.distance_matrix[r][c] = 0
-            else:
-                print("called")
-                top_matrix_distance = self.getCellDistanceToNearestZero(
-                    mat, r-1, c) if (r-1 >= 0 and not (r, c) in self.is_visited) else LARGEST_POSSIBLE_DISTANCE
-                bottom_matrix_distance = self.getCellDistanceToNearestZero(
-                    mat, r+1, c) if (r+1 < rows and not (r, c) in self.is_visited) else LARGEST_POSSIBLE_DISTANCE
-                left_matrix_distance = self.getCellDistanceToNearestZero(
-                    mat,  r, c-1) if (c-1 >= 0 and not (r, c) in self.is_visited) else LARGEST_POSSIBLE_DISTANCE
-                right_matrix_distance = self.getCellDistanceToNearestZero(
-                    mat, r, c+1) if (c+1 < columns and not (r, c) in self.is_visited) else LARGEST_POSSIBLE_DISTANCE
-
-                self.distance_matrix[r][c] = 1 + min(bottom_matrix_distance,
-                                                     right_matrix_distance)
-        else:
-            print("self.distance_matrix is already updated")
-
-        self.is_visited[(r, c)] = True
-        return self.distance_matrix[r][c]
-
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        self.distance_matrix = [[-1] * len(mat[0])] * len(mat)
-        for r in range(len(mat)):
-            for c in range(len(mat[0])):
-                self.getCellDistanceToNearestZero(
-                    mat, r, c)
+        """
+        Algorithm in general: initialize a queue with cells that have the mattrix values equal to zero, then concurrently popped elements from that queue
+        and assigned the cells at position top([i-1][j]), right([i][j+1]), bottom([i+1][j]), left([i][j-1]) with the value cell[i][j] + 1 if the cell
+        at those position haven't been initialized, then add those position to our queue.
 
-        print("The original matrix = ", mat)
+        + In hindsight, we sweep through all cells starting with values zeroes, then assign all adjacent cell the value 1 should they haven't been initialized
+        (have values -1) because that's the smallest distance to a nearby zero they can have.
+        Then, we add those newly assigned elements to the queue to keep track of them and apply the same process above with their adjacent cells.
+        - Time complexity: O(N).
+        - Space complexity: O(N)
+        """
+        row = len(mat)
+        col = len(mat[0])
+        assigned_value_cells = deque()
+        ans = [[-1 for _ in range(col)] for __ in range(row)]
 
-        return self.distance_matrix
+        for i in range(row):
+            for j in range(col):
+                if mat[i][j] == 0:
+                    ans[i][j] = 0
+                    assigned_value_cells.append((i, j))
 
+        while len(assigned_value_cells) > 0:
+            i, j = assigned_value_cells.popleft()
+            curr_distance = ans[i][j]
+            # top
+            if i - 1 >= 0 and ans[i-1][j] == -1:
+                calculate_nearest_zero_distance(
+                    ans, i-1, j, curr_distance, assigned_value_cells)
+            # right
+            if j + 1 < col and ans[i][j+1] == -1:
+                calculate_nearest_zero_distance(
+                    ans, i, j+1, curr_distance, assigned_value_cells)
+            # bottom
+            if i + 1 < row and ans[i+1][j] == -1:
+                calculate_nearest_zero_distance(
+                    ans, i+1, j, curr_distance, assigned_value_cells)
+            # left
+            if j - 1 >= 0 and ans[i][j-1] == -1:
+                calculate_nearest_zero_distance(
+                    ans, i, j-1, curr_distance, assigned_value_cells)
 
-updated_matrix = Solution().updateMatrix([[0, 0, 0], [0, 1, 0], [0, 0, 0]])
-print("updated_matrix = ", updated_matrix)
+        return ans
