dfdc58229d917d373ada954d646b2d1d337031ce
Peter Nguyen <peternguyenforwork@gmail.com>
2023-12-21 14:47:17 -0800

    update: added a cleaner solution for has_cycle

--- hasCycle/my_solution.py
+++ hasCycle/my_solution.py
@@ -1,39 +1,40 @@
 from typing import Optional
-
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         """
-        Pos: position of the node where the cycle starts
-
-        Two pointers technique: Have 2 pointers starting at the same position, but one pointer moves twice as fast as the other. Let's call the the pointer that moves twice as fast two_steps, and the pointer that moves at the normal pace (1 step at a time) one_step.
-        We know that we have a cycle, two_steps will meet one_step at some point during its traversal. Then, we'll return True. Otherwise, once two_steps reach the end of the linked list without hitting one_step, we'll return False.
+        have two pointers- fast and slow. the fast pointer move 2 steps at a time, where the slow pointer moves one step at a time.
+        initialized both fast and slow to head
+        move the fast pointer 2 steps:
+        - step 1: if fast reaches the nullptr. return False.
+        - step 2: if fast reaches the nullptr. return False. otherwise, if fast equals to slow, return True.
+        Move the slow pointer 1 step.
+        Repeat the two steps above while fast != slow.
         """
+        # edge case: empty node or a single node with no cycle: return False
+        if not head or not head.next:
+            return False
 
-        one_step = two_steps = head
-
-        while two_steps and one_step:
-            two_steps = two_steps.next
-            if two_steps != None and one_step == two_steps:
-                # cycle detected!
-                return True
-            if not two_steps:
-                # We've hit the end of the linked list without finding a cycle
+        fast = head
+        slow = head
+        while True:
+            fast = fast.next
+            if not fast:
                 return False
-
-            two_steps = two_steps.next
-            if two_steps != None and one_step == two_steps:
-                # cycle detected!
-                return True
-            if not two_steps:
-                # We've hit the end of the linked list without finding a cycle
+            fast = fast.next
+            if not fast:
                 return False
-            one_step = one_step.next
+            elif fast == slow:
+                return True
+
+            slow = slow.next
 
         return False
