588ff5cb76dd5eaa32478968c5a24f2c4a6f3775
Peter Nguyen <peternguyenforwork@gmail.com>
2024-01-08 15:13:52 -0800

    update: adjusted my solution for lc 209 to be faster

--- min_subarray_length/my_solution.py
+++ min_subarray_length/my_solution.py
@@ -5,49 +5,49 @@ from typing import List
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
         """
-                 8
-               [2,3,1,2]
+        return the minimal length of subarray whose sum is greater than or equal to target. If none, return 0
+                l
+                r
+                0 1 2 3 4 5
+        nums = [9,7,1,2,4,3]
+        target = 7
+        output = 1 ([7])
+
+        sliding window approach:
+        - have a variable called ans to save the length of the valid minimal subarray that we need to return. ans = len(nums) + 1
+        - have left and right pointers. left initialize to 0 and right initialized to 0.
+        - have a variable called sum_so_far initialized to 0 to keep track of the sum within our current window ([left, right]).
+        - repeat these steps while left <= right < len(nums):
+            - repeat while left is smaller than or equal to right and sum_so_far is greater than or equal to target:
+                - ans = min(ans, right - left + 1)
+                - subtract sum_so_far by nums[left].
+                - increment left by 1.
+            - if left is smaller than or equal to right and (sum_so_far < target) => extend the right window:
+                - increment right by 1
+                - if right is smaller than len(nums), increment sum_so_far by nums[right]
+
+
+        return ans if ans is smaller than len(nums) + 1, otherwise return 0.
+
+        Time: O(N) | N = len(nums)
+        Space: O(1)
+                        l
+                          r
         nums = [2,3,1,2,4,3]
         target = 7
-        output = 2
-        subarray [4, 3]
-
-        - have 2 ptrs: left and right
-        - sliding windows: have a curr_sum variable to keep track of the sum of the current subarray within our range.
-        - keep extending to the right if our curr_sum < target. Once curr_sum > target, truncate from the left.
-        - if our curr_sum is greater than or equal to target, compare the current length of our subarray (right - left + 1) to ans, and assign the min of them to ans.
-
-
-        [left, right] = [0, 0]
-        ans = len(nums) + 1
-        curr_sum = nums[left]
-
-        while right < len(nums) and left < len(nums):
-            if curr_sum >= target:
-                subarray_length = right - left + 1
-                ans = min(ans, subarray_length)
-                curr_sum -= nums[left]
-                left += 1
-            else: # curr_sum < target
-                right += 1
-                if right < len(nums):
-                    curr_sum += nums[right]
-
-        return 0 if ans == len(nums) + 1 else ans
         """
-        [left, right] = [0, 0]
+        sum_so_far = nums[0]
         ans = len(nums) + 1
-        curr_sum = nums[left]
+        left = right = 0
 
-        while right < len(nums) and left < len(nums):
-            if curr_sum >= target:
-                subarray_length = right - left + 1
-                ans = min(ans, subarray_length)
-                curr_sum -= nums[left]
+        while left <= right < len(nums):
+            if sum_so_far >= target:  # extend left to reduce our window's size in search for a smaller valid subarray
+                ans = min(ans, right - left + 1)
+                sum_so_far -= nums[left]
                 left += 1
-            else:  # curr_sum < target
+            else:  # sum_so_far < target => extend right
                 right += 1
                 if right < len(nums):
-                    curr_sum += nums[right]
+                    sum_so_far += nums[right]
 
-        return 0 if ans == len(nums) + 1 else ans
+        return ans if ans < len(nums) + 1 else 0
