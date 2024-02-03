2bf92a2ab5e564687103b324c56b10974f5f1bec
Peter Nguyen <peternguyenforwork@gmail.com>
2024-02-03 11:19:08 -0800

    lfu cache is a complicated problem...

--- lfu_cache/my_solution.py
+++ lfu_cache/my_solution.py
@@ -11,6 +11,22 @@ class UseFreqNode:
         self.prev = _prev
         self.next = _next
 
+    def get_lru_key(self) -> int:
+        if len(self.keys_with_use_freq) == 0:
+            raise KeyError("There is no key exists with the current use frequency")
+        
+        return list(self.keys_with_use_freq.keys())[0]
+
+    def add_key(self, _key: int):
+        if _key in self.keys_with_use_freq:
+            self.keys_with_use_freq.move_to_end(_key, last=True)
+        else:
+            self.keys_with_use_freq[_key] = True
+
+    def remove_key(self, _key: int):
+        self.keys_with_use_freq.pop(_key)
+        
+
 class LFUCache:
     """
     This problem's topics are Hash Table, Linked List, Design, and Doubly-Linked List.
@@ -35,8 +51,20 @@ class LFUCache:
         self.use_freq_tail: Optional["UseFreqNode"] = None
 
     # given a key, increment the use frequency of that key by 1.
-    def increment_use_freq(self, key: int) -> None:
-        pass
+    def increment_use_freq(self, _key: int) -> None:
+        # remove the key from the current use_freq
+        # add the key to the next_use_freq (use_freq + 1)
+        if _key not in self.cache or _key not in self.use_freq_lookup:
+            return
+    
+        use_freq_node = self.use_freq_lookup[_key]
+        if use_freq_node:
+            curr_use_freq = use_freq_node.use_freq
+            next_use_freq = curr_use_freq + 1
+            use_freq_node.remove_key(_key)
+            
+
+
     
     # if the key doesn't exist in the cache, return -1
     # otherwise, increment the key's use counter by 1 & return the value at key
@@ -47,21 +75,31 @@ class LFUCache:
         self.increment_use_freq(key)
 
         return self.cache[key]
+    
+    def remove_head(self) -> None:
+        if self.use_freq_head:
+            next_head = self.use_freq_head.next
+            if next_head:
+                next_head.prev = None
+            self.use_freq_head = next_head
         
     # if the key already exists in the cache, update the value at the key.
     # Otherwise (the key doesn't exist in the cache => adding a new key):
     #   - if the cache reaches its capacity (len(self.cache) == self.capacity), remove the key with the smallest use_count. If there are multiple keys with the same smallest use_count, remove the lru key.
     #   - add the key with the value & increment the use_freq at that key.
-    def put(self, key: int, value: int) -> None:
-        if key not in self.cache and len(self.cache) == self.capacity:
+    def put(self, _key: int, _value: int) -> None:
+        if _key not in self.cache and len(self.cache) == self.capacity:
             # remove the key with the smallest use_count and lru
             # the smallest frequency is at self.use_freq_head
-            pass
-        
-        self.cache[key] = value
-        self.increment_use_freq(key)
+            if self.use_freq_head != None:
+                lru_key = self.use_freq_head.get_lru_key()
+                self.use_freq_head.remove_key(lru_key)
 
-# Your LFUCache object will be instantiated and called as such:
-# obj = LFUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+                self.use_freq_lookup.pop(lru_key)
+                self.cache.pop(lru_key)
+
+                if len(self.use_freq_head.keys_with_use_freq) == 0:
+                    self.remove_head()
+        
+        self.cache[_key] = _value
+        self.increment_use_freq(_key)
