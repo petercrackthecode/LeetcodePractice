3d012a615c703e811d5eb8d6aaad7a241ae9cd6f
Peter Nguyen <peternguyenforwork@gmail.com>
2024-02-09 20:44:13 -0800

    update: adjusted my solution to look cleaner based on educative's hints

--- reverse_linked_list/my_iterative_solution.py
+++ reverse_linked_list/my_iterative_solution.py
@@ -9,32 +9,20 @@ class ListNode:
 
 
 class Solution:
-    def reverseList(self, head: Optional["ListNode"]) -> Optional["ListNode"]:
-        """
-        - edge case: if the number of nodes in the linked list is <= 1 (not head or not head.next), return head (we don't need to reverse anything)
-        # the number of nodes in the linked list >= 2
-        - have three nodes: prev, current, and _next. prev = None, current = head, _next = head.next
-        - loop these steps:
-            - current.next = prev
-            - if _next is None => there no more nodes to reverse: break the loop.
-            - otherwise, reassign the nodes to their upcoming positions:
-                prev = current
-                current = _next
-                _next = _next.next
+    class Solution:
+        def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+            """
+            - Initialize the `prev` and `next` pointers to NULL and set the current pointer to the head node.
+            - Traverse the linked list until the current pointer reaches the end of the list.
+            - Within the loop, set the `next` pointer to the next node in the list and reverse the current node’s pointer to point to the previous node.
+            - Update the `prev` and `curr` pointers.
+            - After the loop, the `prev` pointer will point to the last node of the original linked list, so set the head pointer to the `prev` pointer.
+            """
+            prev, curr, next = None, head, None
 
-            return current as the new head
-        # Iterative approach
-        # Time: O(N) | N is the number of nodes in the list
-        # Space: O(1)
-        """
-        if not head or not head.next:
-            return head
-        prev, current, _next = None, head, head.next
+            while curr:  # curr != None
+                next = curr.next
+                curr.next = prev
+                prev, curr = curr, next
 
-        while True:
-            current.next = prev
-            if not _next:
-                break
-            prev, current, _next = current, _next, _next.next
-
-        return current
+            return prev
