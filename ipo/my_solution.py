78a9a77dd4f2a7b6132a20bc8e9e75a039873663
Peter Nguyen <peternguyenforwork@gmail.com>
2024-03-07 13:41:09 -0800

    new: added the boilerplate for lc 502

---
+++ ipo/my_solution.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/ipo/description/
+from typing import Deque, List
+from collections import deque
+from heapq import heappop, heappush
+
+
+class Solution:
+    def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+        pass
