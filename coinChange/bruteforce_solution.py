93e01b6a73448a1c2ac6e792daf1ad1b0e3c4d59
Peter Nguyen <peternguyenforwork@gmail.com>
2024-04-27 10:19:20 -0700

    reimplemented the bruteforce solution for coin change without using a helper function

--- coinChange/bruteforce_solution.py
+++ coinChange/bruteforce_solution.py
@@ -5,35 +5,22 @@ from typing import List
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         """
-        initialize ans to a dummy value: amount + 1 
-        (the maximum amount of coins we can return is amount (amount = 1 cent * amount_time))
-
-        edge case: if the amount is 0, return 0 (we have no way to return 0 from our coin)
-        inorderly select a coin from coins:
-            if the current coin is greater than amount, terminate the loop
-            if the current coin is equal to the amount, assign ans to the minimum between ans and coins_count + 1
-            if the current coin is smaller than the amount, get the remainder equals to amount subtracted by the current coin, the call the coinChangeHelper function with the new amount
-            and new coins_count (coins_count + 1)
-        keep running the loop above until we've reached the end of all coins.
-
-        return -1 if ans > amount else ans
+        given an amount, return the minimum number of coins we can create whose sum is equal to that amount.
+        if that amount of money cannot be made up from any combination of the coins, return -1.
         """
-
         if amount == 0:
             return 0
 
-        ans = amount + 1
+        coins.sort()
 
-        def coinChangeHelper(amount: int, coins_count: int):
-            nonlocal coins, ans
-            for coin in coins:
-                if coin >= amount:
-                    if coin == amount:
-                        ans = min(ans, coins_count + 1)
-                    break
-                else:  # coin < amount
-                    remainder = amount - coin
-                    coinChangeHelper(remainder, coins_count + 1)
+        min_amount: float = float("inf")
 
-        coinChangeHelper(amount, 0)
-        return -1 if ans > amount else ans
+        for coin in coins:
+            if coin > amount:
+                break
+            # coin <= amount
+            remainder: int = amount - coin
+            number_of_coins = 1 + self.coinChange(coins, remainder)
+            min_amount = min(min_amount, number_of_coins)
+
+        return int(min_amount) if min_amount != float("inf") else -1
