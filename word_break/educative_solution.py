53130ca70e7e3bb2dca6e2987e36a0c1e15e3ac0
Peter Nguyen <peternguyenforwork@gmail.com>
2024-05-18 21:43:40 -0700

    added educative's solution

---
+++ word_break/educative_solution.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/word-break/
+from typing import Dict, List
+
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n: int = len(s)
+        word_set: Set[str] = set(wordDict)
+
+        dp: List[bool] = [False] * (n + 1)
+        dp[0] = True
+
+        for i in range(1, n + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in word_set:
+                    dp[i] = True
+                    break
+
+        return dp[n]
