408d21ab57fcd722ce7be28216c4d08967ce82e5
Peter Nguyen <peternguyenforwork@gmail.com>
2024-07-28 11:52:47 -0700

    optimized my solution for lc 658

--- find_k_closest_elements/my_solution.py
+++ find_k_closest_elements/my_solution.py
@@ -1,90 +1,76 @@
 # https://leetcode.com/problems/find-k-closest-elements/
 from typing import List
-from collections import deque
 
 
 class Solution:
-    def find_best_idx(self, arr: List[int], x: int) -> int:
-        [left, right] = [0, len(arr) - 1]
-
-        while left <= right:
-            mid = left + (right-left)//2
-            if mid - 1 < 0 and x <= arr[mid]:
-                return mid
-            elif mid + 1 >= len(arr) and arr[mid] < x:
-                return mid
-            elif arr[mid-1] < x and x <= arr[mid]:
-                return mid
-            elif arr[mid] < x:
-                left = mid + 1
-            else:  # arr[mid] > x
-                right = mid - 1
-
-        return 0
-
-    def is_closer(self, x: int, first: int, second: int) -> bool:
-        first_abs = abs(first - x)
-        second_abs = abs(second - x)
-
-        return (first_abs < second_abs) or (first_abs == second_abs and first < second)
-
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
         """
-        x = 3
-               0 1 2 3 4
-        arr = [1,2,3,4,5]
-                   x
-                 l 
-                   r
+        - arr is sorted.
 
-        [-4, -1, 3]
+                     x
+        arr = [1, 2, 3, 4, 5]
+        k   = 4
+        x   = 3
 
-        - since arr is sorted, we can find an index i where we can place x in arr so that:
-        arr[i-1] < x <= arr[i] (remember to check the boundary for i-1)
-        => custom binary search => have a helper function called find_best_idx(self, arr: List[int], x: int) -> int: that returns the said index.
+        [1, 2, 5, 8, 15] | x = 10
+        [1, 2, 10, 10, 10, 11]
+                        i
 
-        ans = []
-        i = self.find_best_idx(arr, x)
-        [left, right] = [i-1, i]
+        - given the list arr, find the index i where arr[i] <= x < arr[i+1] (if exists) (modified binary search)
+        - have 2 pointers left = i, right = i + 1
+        - loop: while k > 0 and (left >= 0 or right < len(nums)):
+            - if right >= len(nums):
+                - decrement left by 1
+            - otherwise, if left < 0:
+                - increment right by 1.
+            - otherwise, both left and right is in bound:
+                - if is_closer_to_x(arr[left], arr[right]) is true: arr[left] is closer to x than arr[right]:
+                    - decrement left by 1
+                - otherwise:
+                    - increment right by 1
+            - decrement k by 1
 
-        while len(ans) < k and (left >= 0 or right < len(arr)):
-            if left < 0:
-                ans.append(arr[right])
-                right += 1
-            elif right >= len(arr):
-                ans.append(arr[left])
-                left -= 1
-            else:
-                num_left = arr[left]
-                num_right = arr[right]
-                if self.is_closer(x, num_left, num_right):
-                    ans.append(num_left)
-                    left -= 1
-                else:
-                    ans.append(num_right)
-                    right += 1
-
-        return ans
+        - return arr[left+1:right]
         """
-        ans = deque()
-        i = self.find_best_idx(arr, x)
-        [left, right] = [i-1, i]
 
-        while len(ans) < k and (left >= 0 or right < len(arr)):
-            if left < 0:
-                ans.append(arr[right])
-                right += 1
-            elif right >= len(arr):
-                ans.appendleft(arr[left])
+        def find_closest_idx(x: int) -> int:
+            nonlocal arr
+            left, right = 0, len(arr) - 1
+
+            while left <= right:
+                mid: int = left + (right - left) // 2
+                right_bound: int = float("inf") if mid + 1 >= len(arr) else arr[mid + 1]
+                if arr[mid] <= x < right_bound:
+                    return mid
+                elif right_bound <= x:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+
+            # x is smaller than all numbers in arr
+            return 0
+
+        def is_closer_to_x(a: int, b: int) -> bool:
+            nonlocal x
+            a_dist: int = abs(a - x)
+            b_dist: int = abs(b - x)
+
+            return a_dist < b_dist if a_dist != b_dist else a < b
+
+        closest_idx: int = find_closest_idx(x)
+        left, right = closest_idx, closest_idx + 1
+
+        while k > 0 and (left >= 0 or right < len(arr)):
+            if right >= len(arr):
                 left -= 1
+            elif left < 0:
+                right += 1
             else:
-                num_left = arr[left]
-                num_right = arr[right]
-                if self.is_closer(x, num_left, num_right):
-                    ans.appendleft(num_left)
+                if is_closer_to_x(arr[left], arr[right]):
                     left -= 1
                 else:
-                    ans.append(num_right)
                     right += 1
 
-        return list(ans)
+            k -= 1
+
+        return arr[left + 1 : right]
